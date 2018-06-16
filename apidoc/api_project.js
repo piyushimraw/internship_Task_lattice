0948ddcb745812135a4ca5fc794b48310c13c537
piyushimraw <piyush.srivastavaimraw@gmail.com>
2018-06-16 21:52:43 +0530

    Added some minor details

--- apidoc/api_project.js
+++ apidoc/api_project.js
@@ -4,12 +4,20 @@ define({
   "description": "Contains Documentation for APIs exposed for frontend developes",
   "title": "Patient-Doctpr-Interaction",
   "url": "https://localhost:4000/",
+  "header": {
+    "title": "API Documentation",
+    "content": "<h6>Example API Usage</h6>\n"
+  },
+  "footer": {
+    "title": "</>",
+    "content": "<h6>Tests will be added very soon</h6>\n"
+  },
   "sampleUrl": false,
   "defaultVersion": "0.0.0",
   "apidoc": "0.3.0",
   "generator": {
     "name": "apidoc",
-    "time": "2018-06-15T17:23:41.793Z",
+    "time": "2018-06-15T21:34:47.241Z",
     "url": "http://apidocjs.com",
     "version": "0.17.6"
   }
